25ed1756451653db39a4130788ec087ce32dd353
Divyajyoti <sahajdivyajyoti@gmail.com>
2024-11-22 18:29:08 +0000

    add run script

---
+++ run_gen_WF_calc_SNR.sh
@@ -0,0 +1,8 @@
+python gen_WF_calc_SNR.py \
+	--param-file /Users/gravity/ACADEMIC/Projects/Cardiff_University/gwforge/test_pop_day.h5 \
+        --approximant IMRPhenomPv2 \
+        --detectors-and-psds \
+		L1:aLIGOZeroDetHighPower \
+                H1:aLIGOZeroDetHighPower \
+                V1:AdvVirgo \
+        --set-name 1_day_prec_z_10
