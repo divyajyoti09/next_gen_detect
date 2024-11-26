e0b7fe0536a60f8500db0d78a69adda3f861760e
Divyajyoti <sahajdivyajyoti@gmail.com>
2024-11-26 16:45:24 +0000

    modified for memory efficiency

--- gen_WF_calc_SNR_testing_multiprocessing.py
+++ gen_WF_calc_SNR_testing_multiprocessing.py
@@ -383,8 +383,7 @@ if args.num_procs:
     if __name__=='__main__':
         with Pool(args.num_procs) as p:
             print("Generating waveforms")
-            wf_data = p.imap(waveform_gen_base, wf_gen_params_df.to_dict(orient='records'))
-            print(wf_data)
+            wf_data = list(p.imap(waveform_gen_base, wf_gen_params_df.to_dict(orient='records')))
             hpf_data = np.array(wf_data, dtype="object")[:,0]
             hcf_data = np.array(wf_data, dtype="object")[:,1]
             results_dict.update(pd.DataFrame.from_records(np.array(wf_data, dtype="object")[:,2]).to_dict(orient='list'))
