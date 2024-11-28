d8d0b7a9e9295ce2f1b9bb6ca0e50da6c866c8cf
Divyajyoti <sahajdivyajyoti@gmail.com>
2024-11-28 13:43:59 +0000

    minor fix

--- gen_WF_calc_SNR.py
+++ gen_WF_calc_SNR.py
@@ -408,7 +408,7 @@ results_df_chunked = []
 
 if args.num_procs == None:
     for wf_gen_chunk, location_chunk, i in zip(wf_gen_params_df_chunked, location_df_chunked, range(1, n_chunks+1)):
-        print(f"\nProcessing chunk {i}, length = {len(location_chunk['ra'])}")
+        print("\nProcessing chunk {}, length = {}".format(i, len(location_chunk['ra'])))
         print("Generating waveforms")
         wf_data = list(map(waveform_gen_base, wf_gen_chunk.to_dict(orient='records')))
         hpf_data = np.array(wf_data, dtype="object")[:,0]
@@ -448,7 +448,7 @@ if args.num_procs:
     if __name__=='__main__':
         with Pool(args.num_procs) as p:
             for wf_gen_chunk, location_chunk, i in zip(wf_gen_params_df_chunked, location_df_chunked, range(1, n_chunks+1)):
-                print(f'\nProcessing chunk {i}')
+                print("\nProcessing chunk {}, length = {}".format(i, len(location_chunk['ra'])))
                 print("Generating waveforms")
                 wf_data = list(p.map(waveform_gen_base, wf_gen_chunk.to_dict(orient='records')))
                 hpf_data = np.array(wf_data, dtype="object")[:,0]
