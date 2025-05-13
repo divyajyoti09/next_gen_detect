b865e0f66ad0a8445ac31a8ca3f47c5a469fe835
Divyajyoti <sahajdivyajyoti@gmail.com>
2025-05-13 13:18:38 +0100

    gwpopulation scripts added

---
+++ A-sharp_z_peak_mapping/gwpopulation_analysis.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import bilby as bb
+import gwpopulation as gwpop
+import jax
+import matplotlib.pyplot as plt
+import pandas as pd
+from bilby.core.prior import PriorDict, Uniform
+from gwpopulation.experimental.jax import JittedLikelihood, NonCachingModel
+import os
+
+gwpop.set_backend("jax")
+
+xp = gwpop.utils.xp
+
+
+# In[2]:
+
+
+project_dir = '/home/divyajyoti/ACADEMIC/Projects/Cardiff_University/Next_gen_detectability/A-sharp-study/'
+
+
+# ## Load posteriors
+
+# In[3]:
+
+
+posteriors = pd.read_pickle(os.path.join(project_dir, 'gwpopulation', 'BBH', 'redshift_posteriors_499_events.pkl'))
+
+
+# ## Load injections
+
+# In[4]:
+
+
+import dill
+
+with open(os.path.join(project_dir, 'gwpopulation', 'BBH', 'detected_injections.pkl'), "rb") as ff:
+    injections = dill.load(ff)
+
+
+# ## Define models and likelihood
+
+# In[5]:
+
+
+model = NonCachingModel(
+    model_functions=[gwpop.models.redshift.MadauDickinsonRedshift(cosmo_model="Planck18", z_max=8)],
+    #model_functions=[gwpop.models.redshift.PowerLawRedshift(z_max=8)],
+)
+
+vt = gwpop.vt.ResamplingVT(model=model, data=injections, n_events=len(posteriors))
+
+likelihood = gwpop.hyperpe.HyperparameterLikelihood(
+    posteriors=posteriors,
+    hyper_prior=model,
+    selection_function=vt,
+)
+
+
+# ## Define prior
+
+# In[6]:
+
+
+priors = PriorDict()
+priors['gamma'] = Uniform(minimum=1, maximum=5, latex_label="$\\gamma$")
+priors['kappa'] = Uniform(minimum=2, maximum=8, latex_label="$\\kappa$")
+priors['z_peak'] = Uniform(minimum=0.5, maximum=4, latex_label="$z_{peak}$")
+#priors['lamb'] = Uniform(minimum=0.5, maximum=4, latex_label="$\\lambda$")
+
+
+# ## Just-in-time compile
+
+# In[7]:
+
+
+parameters = priors.sample()
+likelihood.parameters.update(parameters)
+likelihood.log_likelihood_ratio()
+get_ipython().run_line_magic('time', 'print(likelihood.log_likelihood_ratio())')
+jit_likelihood = JittedLikelihood(likelihood)
+jit_likelihood.parameters.update(parameters)
+get_ipython().run_line_magic('time', 'print(jit_likelihood.log_likelihood_ratio())')
+get_ipython().run_line_magic('time', 'print(jit_likelihood.log_likelihood_ratio())')
+
+
+# In[8]:
+
+
+result = bb.run_sampler(
+    likelihood=jit_likelihood,
+    priors=priors,
+    sampler="dynesty",
+    nlive=100,
+    label="cosmo",
+    sample="acceptance-walk",
+    naccept=5,
+    save="hdf5",
+)
+
+
+# In[ ]:
+
+
+
+
