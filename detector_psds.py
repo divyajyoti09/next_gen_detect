99974d2088d2e1312307b0388b4d6840c1f38ab7
Divyajyoti <sahajdivyajyoti@gmail.com>
2026-03-10 18:24:17 +0000

    O5 PSD added

--- detector_psds.py
+++ detector_psds.py
@@ -5,17 +5,25 @@ pycbc_psds = psd.analytical.get_psd_model_list()
 
 psds_from_files = {'ASharp':{'file':'./noise_curves/Asharp-asd.txt', # https://dcc.ligo.org/LIGO-T2300041
                              'is_asd':True},
+                   'O5':{'file':'./noise_curves/O5StrainCurves_freqabc-asd.txt', # https://dcc.ligo.org/LIGO-T2500310
+                          'is_asd':True},
                    'CE20':{'file':'./noise_curves/CE20-asd.txt', # https://dcc.cosmicexplorer.org/CE-T2000017-v8/public - cosmic_explorer_20km_strain.txt
                            'is_asd':True}, 
                    'CE40':{'file':'./noise_curves/CE40-asd.txt', 
                            'is_asd':True}, # https://dcc.cosmicexplorer.org/CE-T2000017-v8/public - cosmic_explorer_strain.txt
-                   'ET10_CoBA':{'file':'./noise_curves/18213_ET10kmcolumns.txt', 'is_asd':False}, # https://apps.et-gw.eu/tds/?r=18213
-                   'ET15_CoBA':{'file':'./noise_curves/18213_ET15kmcolumns.txt', 'is_asd':False}, # https://apps.et-gw.eu/tds/?r=18213
-                   'ET20_CoBA':{'file':'./noise_curves/18213_ET20kmcolumns.txt', 'is_asd':False}, # https://apps.et-gw.eu/tds/?r=18213
+                   'ET10_CoBA':{'file':'./noise_curves/18213_ET10kmcolumns.txt', 
+                                'is_asd':False}, # https://apps.et-gw.eu/tds/?r=18213
+                   'ET15_CoBA':{'file':'./noise_curves/18213_ET15kmcolumns.txt', 
+                                'is_asd':False}, # https://apps.et-gw.eu/tds/?r=18213
+                   'ET20_CoBA':{'file':'./noise_curves/18213_ET20kmcolumns.txt', 
+                                'is_asd':False}, # https://apps.et-gw.eu/tds/?r=18213
                   }
 
 def get_available_psds():
-    return(pycbc_psds + list(psds_from_files.keys()))
+    ava_psds = pycbc_psds + list(psds_from_files.keys())
+    ava_psds.remove('O5')
+    ava_psds.extend(['O5a', 'O5b', 'O5c'])
+    return(ava_psds)
 
 def generate_psd(psd_name, length, delta_f, f_low):
     """
@@ -42,12 +50,31 @@ def generate_psd(psd_name, length, delta_f, f_low):
     elif psd_name in psds_from_files.keys():
         if 'ET' in psd_name:
             et_freq, *_, et_psd = np.loadtxt(psds_from_files[psd_name]['file'], unpack=True)
+            if psds_from_files[psd_name]['is_asd']:
+                et_psd = et_psd**2
             psd_data = psd.from_numpy_arrays(et_freq, et_psd,
                                              length, delta_f, f_low)
         else:
             psd_data = psd.from_txt(psds_from_files[psd_name]['file'],
                                     length, delta_f, f_low,
                                     is_asd_file=psds_from_files[psd_name]['is_asd'])
+    elif 'O5' in psd_name:
+        o5_freq, o5a, o5b, o5c = np.loadtxt(psds_from_files['O5']['file'], unpack=True, skiprows=1)
+        if psd_name == 'O5a':
+            if psds_from_files['O5']['is_asd']:
+                o5a = o5a**2
+            psd_data = psd.from_numpy_arrays(o5_freq, o5a,
+                                                length, delta_f, f_low)
+        elif psd_name == 'O5b':
+            if psds_from_files['O5']['is_asd']:
+                o5b = o5b**2
+            psd_data = psd.from_numpy_arrays(o5_freq, o5b,
+                                                length, delta_f, f_low)
+        elif psd_name == 'O5c':
+            if psds_from_files['O5']['is_asd']:
+                o5c = o5c**2
+            psd_data = psd.from_numpy_arrays(o5_freq, o5c,
+                                                length, delta_f, f_low)
 
     else:
         raise ValueError(f"PSD model '{psd_name}' not found.")
