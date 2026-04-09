18998b0296d2a3be65913bc7682a1fecfccb5d3e
Divyajyoti NLN <divyajyoti.nln@ligo.org>
2026-04-09 08:29:37 -0700

    updated

--- A-sharp_z_peak_mapping/gwpopulation/run_gwpop_analysis_Asharp.sh
+++ A-sharp_z_peak_mapping/gwpopulation/run_gwpop_analysis_Asharp.sh
@@ -1,41 +1,12 @@
 source activate gwpopulation
-<<COMMENT
-OUT_DIR1="/home/divyajyoti.nln/Cardiff_University/Next_gen_detectability/A-sharp-study/gwpopulation/BBH/gwpop_analysis_results/Asharp/run09_gwfish_mf_from_opt_SNR_injections_XPHM"
+OUT_DIR1="/home/divyajyoti.nln/Post-O5-study/data/population_analysis/gwpopulation/gwpop_analysis_results/Asharp/run01_gwfish_mf_from_gaussian"
 
 python gwpop_analysis.py \
-	--posterior-file /home/divyajyoti.nln/Cardiff_University/Next_gen_detectability/A-sharp-study/gwpopulation/BBH/posteriors_from_GWFish/LHI_Asharp_2_PLP_z_fref_10_MD_zmax_10_lmrd_22_no_spins_z_posteriors_10K_4057_events_pop_from_XPHM.pkl \
+	--posterior-file /home/divyajyoti.nln/Post-O5-study/data/population_analysis/gwpopulation/posteriors_from_GWFish/LHV_ASharp_2_PLP_z_MD_zmax_10_lmrd_19_no_spins_z_posteriors_10K_1713_events_pop_from_XPHM_fm2_netw.pkl \
 	--out-dir "${OUT_DIR1}" \
-	--detected-injections-file /home/divyajyoti.nln/Cardiff_University/Next_gen_detectability/A-sharp-study/gwpopulation/BBH/injections/LHI_Asharp_SNR_2_pop_PLP_spin_prec_fref_10_z_MD_zmax_10_lmrd_22_corrected_td_detected_injs_mf_from_opt_SNR_12M_points_XPHM.pkl \
+	--detected-injections-file /home/divyajyoti.nln/Post-O5-study/data/population_analysis/gwpopulation/injections/LHV_ASharp_SNR_2_pop_PLP_spin_prec_fref_10_z_MD_zmax_10_lmrd_19_detected_injs_mf_from_gaussian_SNR_30M_points_XPHM.pkl \
 	--label Asharp
 echo " "
 echo "Calcuting variance"
 python post_proc_checks_gwpop.py \
         --out-dir "${OUT_DIR1}"
-COMMENT
-
-OUT_DIR2="/home/divyajyoti.nln/Cardiff_University/Next_gen_detectability/A-sharp-study/gwpopulation/BBH/gwpop_analysis_results/Asharp/run20_gwfish_MDhigh_mf_from_gaussian_SNR_injections_XPHM"
-
-python gwpop_analysis.py \
-	--posterior-file /home/divyajyoti.nln/Cardiff_University/Next_gen_detectability/A-sharp-study/gwpopulation/BBH/posteriors_from_GWFish/LHI_Asharp_4_PLP_set2_z_MDhigh_zp_2.53_zmax_10_lmrd_22_no_spins_z_posteriors_10K_4704_events_pop_from_XPHM_fm2_netw.pkl \
-	--out-dir "${OUT_DIR2}" \
-	--detected-injections-file /home/divyajyoti.nln/Cardiff_University/Next_gen_detectability/A-sharp-study/gwpopulation/BBH/injections/LHI_Asharp_SNR_4_pop_PLP_spin_prec_fref_10_z_MDhigh_zp_2.53_zmax_10_lmrd_22_corrected_td_detected_injs_mf_from_gaussian_SNR_24M_points_XPHM.pkl \
-	--label Asharp
-echo " "
-echo "Calcuting variance"
-python post_proc_checks_gwpop.py \
-        --out-dir "${OUT_DIR2}"
-
-<<COMMENT
-
-OUT_DIR3="/home/divyajyoti.nln/Cardiff_University/Next_gen_detectability/A-sharp-study/gwpopulation/BBH/gwpop_analysis_results/Asharp/run19_gwfish_MDlow_mf_from_opt_SNR_injections_XPHM"
-
-python gwpop_analysis.py \
-	--posterior-file /home/divyajyoti.nln/Cardiff_University/Next_gen_detectability/A-sharp-study/gwpopulation/BBH/posteriors_from_GWFish/LHI_Asharp_5_PLP_z_MDlow_zp_1.54_zmax_10_lmrd_22_no_spins_z_posteriors_10K_3667_events_pop_from_XPHM_fm2_netw.pkl \
-	--out-dir "${OUT_DIR3}" \
-	--detected-injections-file /home/divyajyoti.nln/Cardiff_University/Next_gen_detectability/A-sharp-study/gwpopulation/BBH/injections/LHI_Asharp_SNR_5_pop_PLP_spin_prec_fref_10_z_MDlow_zp_1.54_zmax_10_lmrd_22_corrected_td_detected_injs_mf_from_gaussian_SNR_24M_points_XPHM.pkl \
-	--label Asharp
-echo " "
-echo "Calcuting variance"
-python post_proc_checks_gwpop.py \
-        --out-dir "${OUT_DIR3}"
-COMMENT
